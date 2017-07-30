bcd136e284abeba164b31d4c6f6071a389abde14
Lane Faison <lane.faison@icloud.com>
2017-07-30 00:19:58 -0600

    Initial commit

---
+++ ObserverAndNotificatinPractice/BaseScreen.swift
@@ -0,0 +1,21 @@
+//
+//  BaseScreen.swift
+//  ObserverAndNotificatinPractice
+//
+//  Created by Lane Faison on 7/30/17.
+//  Copyright © 2017 Lane Faison. All rights reserved.
+//
+
+import UIKit
+
+class BaseScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    
+    
+    }
+
+}
+
---
+++ ObserverAndNotificatinPractice/SelectionScreen.swift
@@ -0,0 +1,21 @@
+//
+//  SelectionScreen.swift
+//  ObserverAndNotificatinPractice
+//
+//  Created by Lane Faison on 7/30/17.
+//  Copyright © 2017 Lane Faison. All rights reserved.
+//
+
+import UIKit
+
+class SelectionScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    
+    }
+
+
+}
